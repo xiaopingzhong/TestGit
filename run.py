9100842635e5adfff76ec8a6440a1c510f6b03b5
xiaopingzhong <zhongxiaoping@tju.edu.cn>
2019-05-18 15:22:41 +0800

    修改Pack文件,实现对b,c的加运算

--- run.py
+++ run.py
@@ -5,20 +5,13 @@
 @Time    : 2019-05-18 14:59
 @Author  : zxp
 @Project : python
-@File    : Pack.py
+@File    : run.py
 @Description: ==================================
     
 @license: (C) Copyright 2013-2019.    
 ************************************************
 """
-
-
 def get():
-    b=[]
-    c=[1,2,3]
-    d=b+c
     pass
-
-
 if __name__ == '__main__':
     get()
