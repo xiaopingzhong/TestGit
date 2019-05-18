a007efdfedcb4b28fcb8f87ea2942fcc1c70c6ff
xiaopingzhong <zhongxiaoping@tju.edu.cn>
2019-05-18 15:01:16 +0800

    添加Pack文件

---
+++ Pack.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+************************************************
+@Time    : 2019-05-18 14:59
+@Author  : zxp
+@Project : python
+@File    : Pack.py
+@Description: ==================================
+    
+@license: (C) Copyright 2013-2019.    
+************************************************
+"""
+
+
+def get():
+    pass
+
+
+if __name__ == '__main__':
+    get()
